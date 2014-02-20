93c4f53a3e6a02e7e918c28ebd9c1e79b9aa3fcf
Victor <victor@canelatools.com>
2014-02-20 08:22:37 +0100

    refactoring

--- tv3xml.py
+++ tv3xml.py
@@ -1,9 +1,38 @@
 import webutil
+import xml.etree.ElementTree as ET
 import re
-import tv3xml
+import urlparse
+import urllib
 
 
 letters = ['#','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def _fetch_xml(url):
+    xmlsrc = webutil.fetch_page(url)
+    xmldoc = ET.fromstring(xmlsrc)
+    return xmldoc
+
+
+def fetch_xmlletter(letter):
+    # http://www.tv3.cat/p3ac/llistatProgramesLletra.jsp?lletra=A&page=1&pageItems=1000
+    url = 'http://www.tv3.cat/p3ac/llistatProgramesLletra.jsp?lletra={0}&page=1&pageItems=1000'.format(letter)
+    return _fetch_xml(url)
+
+
+def fetch_xmlinfo(code):
+    url = 'http://www.tv3.cat/pvideo/FLV_bbd_dadesItem.jsp?idint={0}'.format(code)
+    return _fetch_xml(url)
+
+
+def fetch_xmlepisodes(code):
+    # http://www.tv3.cat/p3ac/p3acLlistatVideos.jsp?type=videosprog&id=50338&page=1&pageItems=1000&device=web
+    url = 'http://www.tv3.cat/p3ac/p3acLlistatVideos.jsp?type=videosprog&id={0}&page=1&pageItems=1000&device=web'.format(code)
+    return _fetch_xml(url)
+
+
+def fetch_xmlmedia(code, quality, format):
+    url = 'http://www.tv3.cat/pvideo/FLV_bbd_media.jsp?PROFILE=EVP&ID={0}&QUALITY={1}&FORMAT={2}'.format(code, quality, format)
+    return _fetch_xml(url)
 
 
 def get_letter(letter):
@@ -15,7 +44,7 @@ def get_letter(letter):
         except:
             img = ''
         return {'titol': titol, 'code': code, 'img': img}
-    xmldoc = tv3xml.fetch_xmlletter(letter)
+    xmldoc = fetch_xmlletter(letter)
     list = map(build_item, xmldoc.find('resultats'))
     return list
 
@@ -43,7 +72,7 @@ def get_episodes(code):
                 'plot': plot,
                 'data': data                    
                 }
-    xmldoc = tv3xml.fetch_xmlepisodes(code)
+    xmldoc = fetch_xmlepisodes(code)
     list = map(build_item, xmldoc.find('resultats')) 
     return list
 
@@ -54,13 +83,14 @@ def get_show_info(code):
         quality_label = video.find('qualitat').attrib['label']
         quality_code = video.find('qualitat').text
         return {'format':format, 'quality_label':quality_label, 'quality_code':quality_code}    
-    xmldoc = tv3xml.fetch_xmlinfo(code)
+    xmldoc = fetch_xmlinfo(code)
     title = xmldoc.find('title').text
     videos = map(extract_video_info, xmldoc.find('videos').findall('video'))
     return {'title':title, 'videos':videos}
 
 
 def get_show_rtmp(code, quality_code, format):
-    xmldoc = tv3xml.fetch_xmlmedia(code, quality_code, format)
+    xmldoc = fetch_xmlmedia(code, quality_code, format)
     media = xmldoc.find('item').find('media').text
     return webutil.format_rmtp_url(media)
+
